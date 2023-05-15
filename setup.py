d1a3254b3fd1326306463885bd3c7605f1d850ce
Coldog2333 <a412133593@gmail.com>
2023-05-15 18:46:59 +0900

    v0.0.2

--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_packages
-from setuptools.command.install import install
 
 
 setup(
     name='super_dialseg',
-    version='0.0.1',
+    version='0.0.2',
     description='Supervised Dialogue Segmentation',
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
